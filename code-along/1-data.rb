18a717d77d55ce87028d0af7987345607d8049fa
shireneshou <shirene.shoushtarizadeh@kellogg.northwestern.edu>
2023-01-24 21:10:05 +0000

    code-along

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,43 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+# puts 5
+# puts 2
 
-# Perform simple math with numbers
+# puts 5+2
+# puts 5-2
+# puts 5*2
+# puts 5/2
+
+# puts 5.0/2.0
+
+# # Perform simple math with numbers
+
+# puts 5 * (2 + 1)
 
 # Strings
+puts "Hello, world!"
 
 # Combine strings together
+puts "Tacos are " + "delish"
+puts "Tacos"*3
+
+#puts 3 * Tacos
+puts "Tacos: " + 3.to_s
 
 # Variables
+food = "Tacos"
+qty = 3
+puts food * qty
 
 # Combine strings and variables
+first_name = "Grogu"
+puts "Hello, " + first_name + "!"
+puts "Hello, #{first_name}!"
+greeting = "Hello, #{first_name}!"
+puts greeting
 
+puts "#{food}: #{qty}"
 # String manipulation
+puts first_name.upcase
+puts greeting
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,44 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+is_true = true
+is_false = false
+puts is_true
+puts is_false
 
 # Boolean Expressions
+puts 3 > 2
+puts 3 < 2
+puts 3 == 2
+puts 3!= 2
+
 
 # If Conditional Logic
 
 # If/Else Conditional Logic
+password = "Tacos"
+user_entered_password = "password1"
+if password == user_entered_password
+    puts "you're in!"
+else 
+    puts "go away!"
+end
+
 
 # Elsif Conditional Logic
-
+home_team_score = 101
+away_team_score = 101
+if home_team_score > away_team_score
+    puts "winner!!!"
+elsif home_team_score == away_team_score
+    puts "tied meh.."
+else 
+    puts "loser :("
+end
 # Combining Expressions
+temp = 68
+if temp <80 && if temp > 60
+    puts "it's beautiful!!"
+else
+    puts "brrrr"
+end
